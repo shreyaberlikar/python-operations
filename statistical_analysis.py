bac56888c0a31bf7a044d8f6172549172eb919cd
Shreya Berlikar <138590096+shreyaberlikar@users.noreply.github.com>
2026-07-30 21:17:36 +0530

    Add statistical analysis functions using NumPy

---
+++ statistical_analysis.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.array([10,20,30,40,50])
+
+print("Mean:",np.mean(arr))
+print("Median:",np.median(arr))
+print("Variance:",np.var(arr))
+print("Std:",np.std(arr))
+print("Max:",np.max(arr))
+print("Min:",np.min(arr))
